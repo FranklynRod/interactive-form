992805c2d641d931733e03cb3c6a3e1a00776d19
Franklyn Rodriguez <92125381+FranklynRod@users.noreply.github.com>
2023-09-29 16:04:02 -0400

    apploed revised validation and accessbility javascript functions

--- js/script.js
+++ js/script.js
@@ -20,18 +20,19 @@ jobRole.addEventListener('change', (e) => {
 var design = document.getElementById("design");
 var color = document.getElementById("color");
 var colorOptions = document.querySelectorAll("option[data-theme]")
-
-for (let i = 0; i < colorOptions.length; i++){
-    colorOptions[i].disabled = true;
-}
+color.disabled = true;
 
 design.addEventListener('change', (e) => {
+    color.disabled = false;
+    //loop through color options to set selected to seen by user and unselected to be hidden
     
     for (let i = 0; i < colorOptions.length; i++){
         if (design.value === colorOptions[i].getAttribute("data-theme")){
-            colorOptions[i].disabled = false;
+            colorOptions[i].hidden = false;
+            colorOptions[i].selected = true;
         }else{
-            colorOptions[i].disabled = true;
+            colorOptions[i].hidden = true;
+            colorOptions[i].selected = false;
         }
     }
 });
@@ -44,6 +45,7 @@ var bitCoin = document.getElementById("bitcoin");
 
 payPal.style.display = "none";
 bitCoin.style.display = "none";
+paymentSelect.children[1].setAttribute("selected", true);
 
 paymentSelect.addEventListener('change',(e) => {
     payPal.style.display = "none";
@@ -79,94 +81,77 @@ registerActivities.addEventListener('change',(e) => {
 activitiesCost.textContent = `Total: $0`;
 
 // when user must enter data into input fields according valid rules or else they will be unable to submit form
-
-var nameId = document.getElementById("name");
 var emailAddress = document.getElementById("email");
-var registerActivities = document.getElementById("activities");
 var cardNumber = document.getElementById("cc-num");
 var zipCode =  document.getElementById("zip");
 var cvv =  document.getElementById("cvv");
 var form = document.querySelector("form");
 var paymentSelection = document.getElementById("payment")
-var checkBoxOptions = registerActivities.querySelectorAll('input[type= "checkbox"]')
+var checkBoxOptions = document.querySelectorAll('input[type= "checkbox"]')
 
 
 const isNameValid = (nameId) => {
-    return /[A-Za-z]+$/.test(nameId);
+    return /^[\S\s]+[\S]+$/i.test(nameId.value);
  }
 
- const isEmailValid = (emailAddress) => {
-    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailAddress);
+const isEmailValid = (emailAddress) => {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailAddress.value);
  }
-const isCardValid = (cvv, zipCode, cardNumber) => {
+
+const isCardNumberValid = (cardNumber) => {
     if(paymentSelection.value === "credit-card"){
-        const cardNumberValidation = /^[0-9]{13,16}$/.test(cardNumber);
-        const cvvValidation = /^[0-9]{3}$/.test(cvv);
-        const zipCodeValidation = /^[0-9]{5}$/.test(zipCode);
-
-        return cardNumberValidation && cvvValidation && zipCodeValidation;
+        return /^[0-9]{13,16}$/.test(cardNumber.value);
     }
-    
 };
 
+const isCVVValid = (cvv) => {
+    if(paymentSelection.value === "credit-card"){
+        return /^[0-9]{3}$/.test(cvv.value);
+    }
+};
+
+const isZipCodeValid = (zipCode) => {
+    if(paymentSelection.value === "credit-card"){
+        return  /^[0-9]{5}$/.test(zipCode.value);
+    }
+};
 const isActivityValid = (checkBoxOptions) => {
     return checkBoxOptions.some((checkbox) => checkbox.checked)
 };
 
-form.addEventListener('submit', (e) => {
-    var validName = isNameValid(nameId.value);
-    var validEmail = isEmailValid(emailAddress.value);
-    var validActivity = isActivityValid(checkBoxOptions);
-    var validCard = isCardValid(cvv.value, zipCode.value, cardNumber.value);
-    //check name validation
-    if (!validName || validName === ""){
-        e.preventDefault();
-        validName.parentElement.classList.add("error-border", "not-valid");
-        validName.querySelector('.name-hint').style.display = 'block';
-    } else {
-        validName.parentElement.classList.remove("error-border", "not-valid");
-        validName.parentElement.querySelector('.name-hint').style.display = 'none';
-  }
-    //check activity validation
-    if (!validActivity){
-        e.preventDefault();
-        validActivity.parentElement.classList.add("error-border", "not-valid");
-        validActivity.querySelector('.activities-hint').style.display = 'block';
-    } else {
-        validActivity.parentElement.classList.remove("error-border", "not-valid");
-        validActivity.parentElement.querySelector('.activities-hint').style.display = 'none';
-  };
-    //check email validation
-    if (!validEmail){
-        e.preventDefault();
-        validEmail.parentElement.classList.add("error-border", "not-valid");
-        validEmail.querySelector('.email-hint').style.display = 'block';
-    } else {
-        validEmail.parentElement.classList.remove("error-border", "not-valid");
-        validEmail.parentElement.querySelector('.email-hint').style.display = 'none';
-  };
-    //check card validation
-    if (!validCard){
-        e.preventDefault();
-        validCard.parentElement.classList.add("error-border", "not-valid");
-        validCard.querySelector('.cc-hint').style.display = 'block';
-    } else {
-        validCard.parentElement.classList.remove("error-border", "not-valid");
-        validCard.parentElement.querySelector('.cc-hint').style.display = 'none';
-  }
+form.addEventListener("submit", (e) => {
+    
+    const validator = (inputElement, validationFunction) => {
+        const inputValue = inputElement.value;
+        if (!validationFunction(inputValue)) {
+            e.preventDefault();
+            inputElement.parentElement.classList.remove("valid");
+            inputElement.parentElement.classList.add("error-border", "not-valid");
+            inputElement.nextElementSibling.style.display = "block";  
+        } else {
+            inputElement.parentElement.classList.add("valid");
+            inputElement.parentElement.classList.remove("error-border", "not-valid");
+            inputElement.nextElementSibling.style.display = "none";  
+        }
+    };
+        validator(nameId, isNameValid);
+        validator(emailAddress, isEmailValid);
+        validator(cardNumber, isCardNumberValid);
+        validator(zipCode, isZipCodeValid);
+        validator(cvv, isCVVValid);
+        validator(checkBoxOptions, isActivityValid);
 });
 
 // when user enters invalid information they are given accessible user feedback
+var checkBoxInputs = document.querySelectorAll('input[type= "checkbox"]')
 
-var checkBoxInput = registerActivities.querySelectorAll('input[type= "checkbox"]')
-
-for (let i=0;i< checkBoxInput.length;i++){
-    checkBoxInput[i].addEventListener('focus',(e) =>  {
-        parent = e.target.parentElement;
-        parent.classList.add('focused')
+for (let i=0;i< checkBoxInputs.length;i++){
+    checkBoxInputs[i].addEventListener("focus",(e) =>  {
+        parent = checkBoxInputs[i].parentElement;
+        parent.classList.add("focus")
     });
-    checkBoxInput[i].addEventListener('blur',(e) => {
-        parent = e.target.parentElement;
-        parent.classList.remove('focused')
+    checkBoxInputs[i].addEventListener('blur',(e) => {
+        parent = checkBoxInputs[i].parentElement;
+        parent.classList.remove("focus")
     });
 }
